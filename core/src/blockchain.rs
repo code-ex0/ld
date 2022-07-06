3b4f6259ef77fe850fe513bef3bd4b0551be1f42
code-ex0 <louis.sasse@protonmail.com>
2022-07-06 15:38:23 +0200

    refactor(#2): reformate iterator
    change the file for define the iterator of the blockchain

--- core/src/blockchain.rs
+++ core/src/blockchain.rs
@@ -1,28 +1,9 @@
 use crate::{Block, Payload};
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct Blockchain {
+pub struct Blockchain{
     pub blocks: Vec<Block>,
     pub temp_payload: Vec<Payload>,
-}
-
-pub struct BlockchainIterator {
-    pub blockchain: Blockchain,
-    pub index: usize,
-}
-
-impl Iterator for BlockchainIterator {
-    type Item = Block;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index < self.blockchain.blocks.len() {
-            let block = self.blockchain.blocks[self.index].clone();
-            self.index += 1;
-            Some(block)
-        } else {
-            None
-        }
-    }
 }
 
 impl Blockchain {
@@ -39,17 +20,5 @@ impl Blockchain {
 
     pub fn add_payload(&mut self, payload: Payload) {
         self.temp_payload.push(payload);
-    }
-}
-
-impl IntoIterator for Blockchain {
-    type Item = Block;
-    type IntoIter = BlockchainIterator;
-
-    fn into_iter(self) -> Self::IntoIter {
-        BlockchainIterator {
-            blockchain: self,
-            index: 0,
-        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,19 @@ pub use core::{Block, Payload, Blockchain};
 
 fn main() {
     let mut blockchain = Blockchain::new();
+
+
     let block = Block::new(0, 0, Vec::new(), "".to_string(), 0);
-    blockchain.add_block(block);
-    println!("{:?}", blockchain);
-    println!("Hello, world!");
+    for i in 0..10 {
+        blockchain.add_block(block.clone());
+    }
+
+    blockchain.into_iter().for_each(|mut block| {
+        println!("{:?}", block);
+        block.id += 1;
+    });
+
+    blockchain.into_iter().for_each(|mut block| {
+        println!("{:?}", block);
+    });
 }
