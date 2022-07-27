f9ec85295aaed455f215d1cfb36217a64211aa36
code-ex0 <louis.sasse@protonmail.com>
2022-07-27 15:18:14 +0200

    fix(#3): transaction pool
    optimize lib transaction pool

--- pool/transactions/src/transactions.rs
+++ pool/transactions/src/transactions.rs
@@ -1,8 +1,9 @@
 use std::sync::{Arc, Mutex};
-use core::{Payloads, Payload};
+use core::{Transactions, Transaction};
 
-pub type SyncTransaction = Arc<Mutex<Payloads>>;
+pub type SyncTransaction = Arc<Mutex<Transactions>>;
 
+#[derive(Clone)]
 pub struct TransactionPool {
     pub transactions: SyncTransaction,
 }
@@ -10,16 +11,16 @@ pub struct TransactionPool {
 impl TransactionPool {
     pub fn new() -> TransactionPool {
         TransactionPool {
-            transactions: Arc::new(Mutex::new(Payloads::new())),
+            transactions: Arc::new(Mutex::new(Transactions::new())),
         }
     }
 
-    pub fn add_transaction(&self, transaction: Payload) {
+    pub fn add_transaction(&self, transaction: Transaction) {
         let mut transactions = self.transactions.lock().unwrap();
         transactions.push(transaction);
     }
 
-    pub fn clear_transactions(&self) -> Payloads {
+    pub fn clear_transactions(&self) -> Transactions {
         let mut transactions = self.transactions.lock().unwrap();
         let copy = transactions.clone();
         transactions.clear();
