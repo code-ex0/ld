d845ced573f67f1e92b93c067b92c34ad13f431d
code-ex0 <louis.sasse@protonmail.com>
2022-07-28 13:27:52 +0200

    refactor(#2): update

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,12 @@ use pool_sync::run_in_parallel;
 use pool_miner::Miner;
 use pool_transactions::TransactionPool;
 use pool_api::Api;
+use utils::set_ctrlc_handler;
 
 
 fn main() {
+    set_ctrlc_handler();
+
     let context = Context {
         config: Config {
             port: 8000
@@ -16,7 +19,6 @@ fn main() {
         transactions: TransactionPool::new(),
         blockchain: BlockchainPool::new(),
     };
-
 
     let pool_miner = Miner::new(&context, "Louis Sasse".to_string());
     let pool_api = Api::new(&context);
